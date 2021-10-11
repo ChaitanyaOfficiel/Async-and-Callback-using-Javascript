e2c8a6894557c6ad387d13d7544fd69393e3dfcd
Chaitanya <vankar_chaitanya@yahoo.com>
2021-10-11 19:04:44 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -32,13 +32,13 @@ function createPost(post) {
 
 
 // Async / Await 
-// async function init() {
-//   await createPost( {title:'post two', body:'This is post two'});
+async function init() {
+  await createPost( {title:'post two', body:'This is post two'});
 
-//   getPosts();
-// }
+  getPosts();
+}
 
-// init();
+init();
 
 // Async / Await / fetch 
 async function fetchUsers(){
@@ -50,19 +50,19 @@ async function fetchUsers(){
 
 
 
-
-// createPost({title:'post three', body:'This is post three'})
-// .then(getPosts)
-// .catch(err => console.log(err));
+// uaing promise to check there is error or not 
+createPost({title:'post three', body:'This is post three'})
+.then(getPosts)
+.catch(err => console.log(err));
 
 // creating promise.all
-// const promise1 = Promise.resolve('Hello World!');
-// const promise2 = 10;
-// const promise3 = new Promise((resovle, reject) => 
-//   setTimeout(resolve, 2000, 'GOODBYE')
-// );
-// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+const promise1 = Promise.resolve('Hello World!');
+const promise2 = 10;
+const promise3 = new Promise((resovle, reject) => 
+  setTimeout(resolve, 2000, 'GOODBYE')
+);
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
-// Promise.all([promise1,promise2,promise3,promise4])
-// .then(values => console.log(values));
+Promise.all([promise1,promise2,promise3,promise4])
+.then(values => console.log(values));
 
