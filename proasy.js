cfef1906b6a4c0fb2db4697c0a510e9135d8982b
Chaitanya <vankar_chaitanya@yahoo.com>
2021-12-19 22:52:25 +0530

    Update proasy.js

--- proasy.js
+++ proasy.js
@@ -1,33 +1,33 @@
-// let stocks = {
-//   Fruits : ["Strawberry", "Grapes", "Banana", "Apple"],
-//   Liquid : ["water", "ice"],
-//   Holder : ["cone","cup","stick"],
-//   Toppings : ["chocolate", "peanuts"]
-// };
+let stocks = {
+  Fruits : ["Strawberry", "Grapes", "Banana", "Apple"],
+  Liquid : ["water", "ice"],
+  Holder : ["cone","cup","stick"],
+  Toppings : ["chocolate", "peanuts"]
+};
 
-// let is_shop_open = true;
+let is_shop_open = true;
 
-// let toppings_choice = () => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(
-//  console.log("Which topping would you love? ")
-//       );
-//     }, 3000);
-//   });
-// };
+let toppings_choice = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+ console.log("Which topping would you love? ")
+      );
+    }, 3000);
+  });
+};
 
-// async function kitchen(){
-//   console.log("A");
-//    console.log("B");
-//     console.log("C");
-//      console.log("D");
-//      await toppings_choice();
-//       console.log("E");
-//        console.log("F");
-// }
-// kitchen();
+async function kitchen(){
+  console.log("A");
+   console.log("B");
+    console.log("C");
+     console.log("D");
+     await toppings_choice();
+      console.log("E");
+       console.log("F");
+}
+kitchen();
 
-// console.log("doing the dishes");
-// console.log("cleaning the tables");
-// console.log("taking others orders");
+console.log("doing the dishes");
+console.log("cleaning the tables");
+console.log("taking others orders");
