84f20b934545ed2a1894e7d051348c9912bdafd4
Chaitanya <vankar_chaitanya@yahoo.com>
2021-12-19 22:53:06 +0530

    Update synch.js

--- synch.js
+++ synch.js
@@ -1,22 +1,22 @@
 //synchoronus
 
-// console.log("I");
-// console.log("am ");
-// setTimeout(() => {
-//   console.log("going to");
-// }, 4000);
-// console.log("Work ");
+console.log("I");
+console.log("am ");
+setTimeout(() => {
+  console.log("going to");
+}, 4000);
+console.log("Work ");
 
 
-// callback function
-// function one(call_two){
-//    call_two();
-//   console.log(" step One complete. please call step 2 ");
+callback function
+function one(call_two){
+   call_two();
+  console.log(" step One complete. please call step 2 ");
  
-// }
-// function two(){
+}
+function two(){
  
-//   console.log(" step two ")
-// }
-// one(two);
-// stock for ice cream
+  console.log(" step two ")
+}
+one(two);
+stock for ice cream
